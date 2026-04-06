0342ad1e19f019b6779730fd04c109767a1e381c
ihsankhan-752 <ihsan@example.com>
2026-04-06 17:17:27 +0400

    Route Order Fixed

--- index.js
+++ index.js
@@ -8,14 +8,17 @@ import adminRouter from "./routes/admin.router.js";
 import commentRouter from "./routes/comment.router.js";
 
 app.use(express.json());
-app.use("/user", userRouter);
-app.use("/tickets", ticketRouter);
-app.use("/admin", adminRouter);
-app.use("/comments", commentRouter);
 
 app.get("/api/health", (req, res) => {
   return res.status(200).json({ message: "Production Level Done" });
 });
+
+
+
+app.use("/user", userRouter);
+app.use("/tickets", ticketRouter);
+app.use("/admin", adminRouter);
+app.use("/comments", commentRouter);
 
 app.listen(PORT, (req, res) => {
   console.log(`Server is Up and Running on PORT: ${PORT}`);
